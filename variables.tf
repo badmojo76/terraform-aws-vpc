fdc2911f1d578c2f7e5cad76f21d6c15b4c4fb93
Kurt Bachelder <kurt.bachelder@gmail.com>
2021-02-22 22:50:56 -0500

    removing variables

--- variables.tf
+++ variables.tf
@@ -2,9 +2,10 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 */
+/*
 variable "aws_key_path" {}
 variable "aws_key_name" {}
-
+*/
 variable "vpc_cidr" {
     description = "CIDR for the whole VPC"
     default = "10.0.0.0/16"
