9fa4422aa1ce04d261a69e9036435cd91dbb885c
EC2 Default User <ec2-user@ip-172-31-7-121.ec2.internal>
2021-02-23 05:40:46 +0000

    provider update

--- aws.tf
+++ aws.tf
@@ -1,9 +1,17 @@
 provider "aws" {
-    alias  = "us-east-1"
-    region = "us-east-1"
+  alias = "useast1"
+  region = "us-east-1"
+  skip_credentials_validation = true
+  skip_region_validation      = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms      = true
 }
 
 provider "aws" {
-    alias  = "us-east-2"
-    region = "us-east-2"
+  alias = "useast2"
+  region = "us-east-2"
+  skip_credentials_validation = true
+  skip_region_validation      = true
+  skip_requesting_account_id  = true
+  skip_get_ec2_platforms      = true
 }
