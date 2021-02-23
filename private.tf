1e761982289034ddec92a7267c7d4df297c30328
Kurt Bachelder <kurt.bachelder@gmail.com>
2021-02-22 22:54:34 -0500

    provider aliases

--- private.tf
+++ private.tf
@@ -2,7 +2,7 @@
   Database Servers
 */
 resource "aws_security_group" "db" {
-    provider = us-east-1
+    provider = aws.us-east-1
     name = "vpc_db"
     description = "Allow incoming database connections."
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -2,6 +2,8 @@
 aws_access_key = ""
 aws_secret_key = ""
 */
+/*
 aws_key_path = ""
 aws_key_name = ""
 
+*/
