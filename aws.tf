706cbe24e410732cf63aae2f24f224bee558ac03
Kurt Bachelder <kurt.bachelder@gmail.com>
2021-02-22 22:34:37 -0500

    provider update

--- aws.tf
+++ aws.tf
@@ -1,15 +1,17 @@
-provider "aws-useast1" {
+provider "aws" {
 /*
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
 */
+    alias  = "us-east-1"
     region = "us-east-1"
 }
 
-provider "aws-useast2" {
+provider "aws" {
 /*
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
 */
+    alias  = "us-east-2"
     region = "us-east-2"
 }
