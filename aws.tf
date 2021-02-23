6587760014f262fea42828878661c3e87adad5bb
Kurt Bachelder <kurt.bachelder@gmail.com>
2021-02-22 22:35:35 -0500

    cleanup

--- aws.tf
+++ aws.tf
@@ -1,17 +1,9 @@
 provider "aws" {
-/*
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-*/
     alias  = "us-east-1"
     region = "us-east-1"
 }
 
 provider "aws" {
-/*
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-*/
     alias  = "us-east-2"
     region = "us-east-2"
 }
